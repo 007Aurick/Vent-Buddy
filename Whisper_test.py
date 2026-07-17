562816d5feb530ec10b9a200e3ce335571c10e5c
Aurick Anwar <aurick.anwar2260biga@gmail.com>
2026-07-17 13:25:55 -0400

    Got Basic Speech to Text working

--- Whisper_test.py
+++ Whisper_test.py
@@ -1,6 +1,18 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
 import whisper
 
-model = whisper.load_model("base")
-result = model.transcribe("audio.wav")
+sampleRate = 44100
 
+
+duration = 5
+
+print("Recording...")
+recording = sd.rec(int(duration * sampleRate), samplerate=sampleRate, channels=2)
+sd.wait()
+write("output.wav", sampleRate, recording)
+print("Recording complete. Saved as output.wav")
+
+model = whisper.load_model("base")
+result = model.transcribe("output.wav")
 print(result["text"])
