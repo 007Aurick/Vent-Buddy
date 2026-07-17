71edf5124f4e37d568093c8cc3d3c296e66e16b5
Aurick Anwar <aurick.anwar2260biga@gmail.com>
2026-07-17 16:24:02 -0400

    Finished the basic features

--- Whisper_test.py
+++ Whisper_test.py
@@ -1,18 +1,28 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
+import speech_recognition as sr
 import whisper
+import sounddevice as sd
+import soundfile as sf
 
-sampleRate = 44100
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Adjusting for ambient noise...")
+    r.adjust_for_ambient_noise(source,duration=1)
+    print("Listening...")
+    audio = r.listen(source)
 
 
-duration = result["text"]
-
-print("Recording...")
-recording = sd.rec(int(duration * sampleRate), samplerate=sampleRate, channels=2)
-sd.wait()
-write("output.wav", sampleRate, recording)
-print("Recording complete. Saved as output.wav")
+with open("output.wav", "wb") as f:
+    f.write(audio.get_wav_data())
 
 model = whisper.load_model("base")
 result = model.transcribe("output.wav")
-print(result["text"])
+
+data, samplerate = sf.read("output.wav")
+print("Playing back the recorded audio...")
+sd.play(data, samplerate)
+sd.wait()
+
+
+
+
